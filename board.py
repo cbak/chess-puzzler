6343c42c8bf81e87672de0c6ab9df4de72556d9c
Chris Bak <cb574@york.ac.uk>
2018-08-31 15:41:41 +0100

    Renamed class to ChessPosition and added fields

--- board.py
+++ board.py
@@ -1,21 +1,23 @@
 import sys
 
-class ChessBoard:
-   """Represents a chess board."""
+class ChessPosition:
+   """Represents a static chess position."""
 
-   def __fen_to_board__(self, fen): 
+   def __fen_to_position__(self, fen): 
       """Converts an FEN string into the chess position it represents."""
-      self.board = [["-"] * 8 for i in range(8)]
+      self.position = [["-"] * 8 for i in range(8)]
       rows = fen.split("/")
-      # Truncate the final row to remove the remainder of the FEN string.
-      rows[7] = rows[7].split(" ")[0]
+      # The last string contains the piece data for the last row followed
+      # by the metadata. 
+      data = rows[7].split(" ") 
+      rows[7] = data[0]
       for cur_row, row in enumerate(rows):
          if len(row) > 8:
             sys.exit("Error: Too many characters in a row")
          cur_col = 0
          for c in row:
             if c.isalpha():
-               self.board[cur_row][cur_col] = c
+               self.position[cur_row][cur_col] = c
                cur_col += 1
             elif c.isdigit():
                cur_col += int(c)
@@ -23,20 +25,36 @@ class ChessBoard:
                print("Error: Invalid character %c." % (c))
                break
             if cur_col > 8:
-               sys.exit("Error: Invalid row (%d): too many columns accounted for." % (cur_row))
+               sys.exit("Error: Too many columns in row %d." % (cur_row))
          cur_row += 1
+      self.turn = data[1]
+      self.castling = data[2]
+      self.en_passant = data[3]
+      self.fifty_move = data[4]
+      self.move_number = data[5]
+
+   __fen_start__ = ("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR "
+                    "w KQkq - 0 1")
             
-   def __init__(self, fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"):
-      """Initialises a 2-dimensional array representing the
-         starting position of a chess game."""
-      self.__fen_to_board__(fen)
+   def __init__(self, fen = __fen_start__):
+      """Initialises a 2-dimensional array from an FEN string. The default
+         FEN describes the starting position of a chess game."""
+      self.__fen_to_position__(fen)
 
-
-   def print_board(self):
-      """Prints the chess board"""
-      for row in self.board:
+   def print_position(self):
+      """Prints the chess position"""
+      print(" ")
+      for row in self.position:
          print(row)
+      print(" ")
+      print("%s to move." % self.turn.upper())
+      if self.castling == "-":
+         print("Neither side can castle.")
+      else:
+         print("Castling: %s" % self.castling)
+      if self.en_passant != "-":
+         print("En passant available on %s" % self.en_passant)
 
-board1 = ChessBoard("5k2/8/8/8/4p3/3p4/1K6/7Q w - - 0 56")
-board1.print_board()
+position1 = ChessPosition()
+position1.print_position()
 
